8a5debfc4ea78e1964c6fef4430dbb0263a1199c
nikhithak <nikhithak24@gmail.com>
2016-03-04 14:27:07 -0600

    Simple iOS App

---
+++ MacysTest/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  MacysTest
+//
+//  Created by Nikhitha Akula on 3/4/16.
+//  Copyright © 2016 Nikhitha Akula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ MacysTest/ViewController.h
@@ -0,0 +1,18 @@
+//
+//  ViewController.h
+//  MacysTest
+//
+//  Created by Nikhitha Akula on 3/4/16.
+//  Copyright © 2016 Nikhitha Akula. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AFHTTPRequestOperationManager.h"
+#import "AFNetworking.h"
+#import "MBProgressHUD.h"
+
+@interface ViewController : UIViewController<UITableViewDataSource,UITableViewDelegate,UITextFieldDelegate>
+
+
+@end
+
